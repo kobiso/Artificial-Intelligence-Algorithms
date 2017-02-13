07e68dfb17125404239fcd0560ff7db41c60cd11
kobiso_Surface pro4 <kobiso62@gmail.com>
2017-02-13 17:15:16 +0900

    Implemented 'flood_fill' algorithm

--- 1. A_Star Algorithm/grid_algo.py
+++ 1. A_Star Algorithm/grid_algo.py
@@ -19,3 +19,37 @@ class Grid:
         temp_grid = [list(map(int,line.strip())) for line in open(filename, 'r')]
         # transpose the input since we read it in as (y, x)
         self.__grid = [list(i) for i in zip(*temp_grid)]
+
+    # return the cost of a given action
+    # note: this only works for actions in our LEGAL_ACTIONS defined set (8 directions)
+    def __get_action_cost(self, action):
+        return CARDINAL_COST if (action[0] == 0 or action[1] == 0) else DIAGONAL_COST
+
+    # returns the tile type of a given position
+    def get(self, tile): return self.__grid[tile[0]][tile[1]]
+    def width(self):     return self.__width
+    def height(self):    return self.__height
+
+    # TODO: Implement ComputeSectors and flood_fill method
+    def compute_sectors(self):
+        self.sectors = np.zeros((MAX_SIZE + 1, self.width(), self.height()))
+        self.sector_num = np.zeros(4)
+        for size in range(1, MAX_SIZE + 1):
+            for x in range(self.width()):
+                for y in range(self.height()):
+                    self.sector_num[size] += 1
+                    self.flood_fill((x, y), self.get((x, y)), size)
+
+    def flood_fill(self, pos, type, size):
+        try:
+            if self.sectors[size][pos[0]][pos[1]] != 0: return
+            for i in range(size):
+                for j in range(size):
+                    if self.get((pos[0] + i, pos[1] + j)) != type: return
+        except IndexError:
+            return
+        self.sectors[size][pos[0]][pos[1]] = self.sector_num[size]
+        self.flood_fill((pos[0] + 1, pos[1]), type, size)
+        self.flood_fill((pos[0] - 1, pos[1]), type, size)
+        self.flood_fill((pos[0], pos[1] + 1), type, size)
+        self.flood_fill((pos[0], pos[1] - 1), type, size)
